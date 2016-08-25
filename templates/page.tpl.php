323734f18c38eb352057163335ce87783314286e
palagpatski <palagpatski@gmail.com>
2016-08-25 13:25:14 +0800

    Disable some regions and modifications to the template

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -21,7 +21,39 @@
 </header>
 
 <main id="page">
-  <div class="page-inner">
+
+  <?php if (!empty($page['highlighted'])) : ?>
+    <section id="highlighted">
+      <div class="highlighted-inner">
+        <div id='page-title'><div class='limiter clearfix'>
+          <div class='tabs clearfix'>
+            <?php if ($primary_local_tasks): ?>
+              <ul class='primary-tabs links clearfix'><?php print render($primary_local_tasks) ?></ul>
+            <?php endif; ?>
+          </div>
+          <?php print render($title_prefix); ?>
+          <h1 class='page-title <?php print $page_icon_class ?>'>
+            <?php if (!empty($page_icon_class)): ?><span class='icon'></span><?php endif; ?>
+            <?php if (arg(0) == 'user' && arg(1) == 'login') : ?>
+              <h1 class="page-title"> Log In to <?php print strip_tags($site_name); ?> </h1>
+            <?php elseif (arg(0) == 'user' && arg(1) == 'password') : ?>
+              <h1 class="page-title"> Reset Your <?php print strip_tags($site_name); ?> Password </h1>
+            <?php else : ?>
+              <?php if ($title) print $title ?>
+            <?php endif ; ?>
+          </h1>
+          <?php if ($action_links): ?>
+            <ul class='action-links links clearfix'><?php print render($action_links) ?></ul>
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
+        </div></div>
+
+        <?php if ($show_messages && $messages): ?>
+          <div id='console'><div class='limiter clearfix'><?php print $messages; ?></div></div>
+        <?php endif; ?>
+      </div>
+    </section>
+  <?php else: ?>
     <div id='page-title'><div class='limiter clearfix'>
       <div class='tabs clearfix'>
         <?php if ($primary_local_tasks): ?>
@@ -43,21 +75,23 @@
         <ul class='action-links links clearfix'><?php print render($action_links) ?></ul>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
-    </div></div>
+    </div>
 
     <?php if ($show_messages && $messages): ?>
-    <div id='console'><div class='limiter clearfix'><?php print $messages; ?></div></div>
+      <div id='console'><div class='limiter clearfix'><?php print $messages; ?></div></div>
     <?php endif; ?>
+  <?php endif; ?>
 
-    <div id='main-content' class='limiter clearfix'>
-      <?php if ($page['help']) print render($page['help']) ?>
-      <div id='content' class='page-content clearfix'>
-        <?php if (!empty($page['content'])) print render($page['content']) ?>
-      </div>
+  <div id='main-content' class='limiter clearfix'>
+    <?php if ($page['help']) print render($page['help']) ?>
+    <div id='content' class='page-content clearfix'>
+      <?php if (!empty($page['content'])) print render($page['content']) ?>
     </div>
   </div>
 </main>
 
-<div id='footer' class='clearfix'>
-  <?php print render($page['footer']); ?>
-</div>
+<footer> id='footer' class='clearfix'>
+  <div class="footer-inner">
+    <?php print render($page['footer']); ?>
+  </div>
+</footer>

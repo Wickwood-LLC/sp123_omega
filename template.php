837e91a3d4c91406b78a225245542312cef93c54
palagpatski <palagpatski@gmail.com>
2016-08-26 17:46:09 +0800

    Hide page title

--- template.php
+++ template.php
@@ -5,9 +5,3 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * SP123 Omega theme.
  */
-
-function sp123_omega_preprocess_page(&$variables) {
-	if ($variables['node']->type == 'panopoly_landing_page') {
-		$variables['title'] = NULL; // Hide the node title before the node is getting rendered.
-	}
-}

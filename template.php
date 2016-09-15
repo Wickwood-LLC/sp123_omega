b7c118760a72a9f33ba7d0f4a2dbf0d758da4257
palagpatski <palagpatski@gmail.com>
2016-09-15 14:27:38 +0800

    Add viewport meta tag

--- template.php
+++ template.php
@@ -5,3 +5,14 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * SP123 Omega theme.
  */
+
+function sp123_omega_preprocess_html(&$vars) {
+  $viewport = array(
+   '#tag' => 'meta',
+   '#attributes' => array(
+     'name' => 'viewport',
+     'content' => 'width=device-width, initial-scale=1, user-scalable=yes',
+   ),
+  );
+  drupal_add_html_head($viewport, 'viewport');
+}

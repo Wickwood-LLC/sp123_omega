a49969d87a9bf8ce47ff8d7dfc627201ca2e2cc6
palagpatski <palagpatski@gmail.com>
2016-10-27 12:13:52 +0800

    Fixing the Archive nav

--- js/sp123-omega.behaviors.js
+++ js/sp123-omega.behaviors.js
@@ -82,12 +82,4 @@
     }
   };
 
-  Drupal.behaviors.archiveNav = {
-    attach: function (context, settings) {
-      if ( $.trim( $('.blog-type-page .view-display-id-panel_pane_2 .pager li').text() ).length === 0 ) {
-        $('.blog-type-page .view-display-id-panel_pane_2 .pager li').addClass('empty');
-      }
-    }
-  };
-
 })(jQuery);

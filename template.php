8474603ac6425fbb9fc3327995461e46c861f703
palagpatski <palagpatski@gmail.com>
2016-11-15 16:31:44 +0800

    dpm

--- template.php
+++ template.php
@@ -127,5 +127,7 @@ function sp123_omega_form_element_label($variables) {
 	else {
 	  return ' <label' . drupal_attributes($attributes) . '>' . $t('!title !required', array('!title' => $title, '!required' => $required)) . "</label>\n";
 	}
+
+	dpm($variables['rendered_element']);
 }
 

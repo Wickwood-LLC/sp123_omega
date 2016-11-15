7b0195d508d6c0cca0fc024562de9c40d32f63d5
palagpatski <palagpatski@gmail.com>
2016-11-15 15:56:27 +0800

    Checkbox styles

--- template.php
+++ template.php
@@ -129,17 +129,3 @@ function sp123_omega_form_element_label($variables) {
 	}
 }
 
-function sp123_omega_checkbox($variables) {
-  $element = $variables['element'];
-  $element['#attributes']['type'] = 'checkbox';
-  element_set_attributes($element, array('id', 'name', '#return_value' => 'value'));
-
-  // Unchecked checkbox has #value of integer 0.
-  if (!empty($element['#checked'])) {
-    $element['#attributes']['checked'] = 'checked';
-  }
-  _form_set_class($element, array('form-checkbox', 'checkbox'));
-
-  return '<input' . drupal_attributes($element['#attributes']) . ' />';
-}
-

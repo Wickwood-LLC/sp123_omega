4caa55f859f38d564ecf7fa059cc9df985ad193c
palagpatski <palagpatski@gmail.com>
2017-04-27 10:19:41 +0800

    Function override to display the breadcrumb only when there are two or more items in the trail.

--- template.php
+++ template.php
@@ -166,7 +166,15 @@ function sp123_omega_menu_local_tasks(&$variables) {
 }
 
 function sp123_omega_breadcrumb($variables) {
-  if (count($variables['breadcrumb']) > 1) {
-    return implode($variables['breadcrumb']);
+  $breadcrumb = $variables['breadcrumb'];
+
+  if (count($variables['breadcrumb']) > 1) {  // Only display the breadcrumb if there are more than one items in the trail
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+    // Override Drupal core breadcrumb item imploding (no arrow).
+    $output .= '<ul class="breadcrumb"><li>' . implode('</li><li>', $breadcrumb) . '</li></ul>';
+    return $output;
   }
 }

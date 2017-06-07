1b6fb3db89a79dfdda57a161ba447aabfc14eab6
palagpatski <palagpatski@gmail.com>
2017-06-07 16:06:47 +0800

    Adjust computation

--- js/sp123-omega.behaviors.js
+++ js/sp123-omega.behaviors.js
@@ -97,7 +97,7 @@
       var currentPosition;
       var $this;
 
-      $(window).on("load resize", function() {
+      $(window).on("load resize scroll", function() {
         $('#block-superfish-1').each(function() {
           $this = $('#block-superfish-1');
 
@@ -120,17 +120,17 @@
         }); 
       });
 
-      $(window).scroll(function(){ // scroll event 
-        console.log(stickyTop);
-        console.log(currentPosition);
+      // $(window).scroll(function(){ // scroll event 
+      //   console.log(stickyTop);
+      //   console.log(currentPosition);
 
-        if (currentPosition < 0) {   // if target element goes above the screen
-          $this.css({ position: 'fixed', top: '65px', bottom: 'initial', width: $this.parent().width() });   //stick it at the top
-        }
-        else {
-          $this.css({ position: 'static', width: menuWidth });
-        }
-      });
+      //   if (currentPosition < 0) {   // if target element goes above the screen
+      //     $this.css({ position: 'fixed', top: '65px', bottom: 'initial', width: $this.parent().width() });   //stick it at the top
+      //   }
+      //   else {
+      //     $this.css({ position: 'static', width: menuWidth });
+      //   }
+      // });
     }
   };
 

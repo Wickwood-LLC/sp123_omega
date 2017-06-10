adcdc25f7d6ac34e3b74155c42e71d6fe74552e5
palagpatski <palagpatski@gmail.com>
2017-06-10 12:22:26 +0800

    Change the resize library to mResize from http://manos.malihu.gr/event-based-jquery-element-resize/

--- js/sp123-omega.behaviors.js
+++ js/sp123-omega.behaviors.js
@@ -153,8 +153,7 @@
       }
 
       $(window).on("load resize", reset());
-      $('#admin-menu').resize(reset());
-      $('#admin-menu').removeResize(reset());
+      $('#admin-menu').on("mresize", reset());
 
       $(window).scroll(function(){ // scroll event 
         windowTop = $(window).scrollTop();    // tells how far our screen is currently from the top of the page
